0c83792dd440617ae757012a9c76cd65e52f43e7
Yes-and <72066894+Yes-and@users.noreply.github.com>
2026-03-18 19:05:06 +0000

    Remove dead code

--- src/models/dnn.py
+++ src/models/dnn.py
@@ -23,30 +23,10 @@ def build_dnn(input_dim, num_classes):
 
     model.add(Dense(num_classes, activation='softmax'))
 
-    optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=0.0005)
-
-    loss = tf.keras.losses.SparseCategoricalCrossentropy()
-
     model.compile(
-        optimizer=optimizer,
-        loss=loss,
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
         metrics=['accuracy']
     )
 
     return model
-
-def focal_loss(gamma=2., alpha=0.25):
-    def loss(y_true, y_pred):
-        y_true = tf.cast(y_true, tf.int32)
-        y_true = tf.one_hot(y_true, depth=tf.shape(y_pred)[-1])
-
-        epsilon = 1e-7
-        y_pred = tf.clip_by_value(y_pred, epsilon, 1. - epsilon)
-
-        cross_entropy = -y_true * tf.math.log(y_pred)
-        weight = alpha * tf.pow(1 - y_pred, gamma)
-
-        loss = weight * cross_entropy
-        return tf.reduce_mean(tf.reduce_sum(loss, axis=1))
-
-    return loss
